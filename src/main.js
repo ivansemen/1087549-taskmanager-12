29d52dbf47f01b57d359c47315a892fd9b1bf79b
Ivan <vsem2004@gmail.com>
2020-08-06 19:03:34 +0300

    M2T1 Revisions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import {createSiteMenuTemplate} from "./view/site-menu.js";
-import {createFilterTemplate} from "./view/filter.js";
-import {createBoardTemplate} from "./view/board.js";
-import {createSortTemplate} from "./view/sort.js";
-import {createTaskTemplate} from "./view/task.js";
-import {createTaskEditTemplate} from "./view/task-edit.js";
-import {createLoadMoreButtonTemplate} from "./view/button-load.js";
+import {createMenuTemplate} from "./view/site-menu";
+import {createFilterTemplate} from "./view/filter";
+import {createBoardTemplate} from "./view/board";
+import {createSortingTemplate} from "./view/sort";
+import {createTaskTemplate} from "./view/task";
+import {createTaskEditorTemplate} from "./view/task-edit";
+import {createLoadMoreButtonTemplate} from "./view/button-load";
 
 const TASK_COUNT = 3;
 
@@ -15,15 +15,15 @@ const render = (container, template, place) => {
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+render(siteHeaderElement, createMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
 const boardElement = siteMainElement.querySelector(`.board`);
 const tasksElement = boardElement.querySelector(`.board__tasks`);
 
-render(boardElement, createSortTemplate(), `afterbegin`);
-render(tasksElement, createTaskEditTemplate(), `beforeend`);
+render(boardElement, createSortingTemplate(), `afterbegin`);
+render(tasksElement, createTaskEditorTemplate(), `beforeend`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
   render(tasksElement, createTaskTemplate(), `beforeend`);
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,4 +1,4 @@
-export const createSiteMenuTemplate = () => {
+export const createMenuTemplate = () => {
   return `<section class="control__btn-wrap">
       <input
         type="radio"
