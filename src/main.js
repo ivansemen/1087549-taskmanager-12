1cc4df1d57286a6353fa5b59d55c0dfd48515a1d
Ivan <vsem2004@gmail.com>
2020-08-12 23:20:57 +0300

    M3T1 Used task mocks

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import {createSortingTemplate} from "./view/sort";
 import {createTaskTemplate} from "./view/task";
 import {createTaskEditorTemplate} from "./view/task-edit";
 import {createLoadMoreButtonTemplate} from "./view/button-load";
+import {generateTask} from "./mock/task";
 
 const TASK_COUNT = 3;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -26,7 +29,7 @@ render(boardElement, createSortingTemplate(), `afterbegin`);
 render(tasksElement, createTaskEditorTemplate(), `beforeend`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(tasksElement, createTaskTemplate(), `beforeend`);
+  render(tasksElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
