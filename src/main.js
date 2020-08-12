3f1e854a750460598769604ec1c7263df9105ea0
Ivan <vsem2004@gmail.com>
2020-08-12 23:41:16 +0300

    M3T1 Generated data for a form component

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import {createTaskEditorTemplate} from "./view/task-edit";
 import {createLoadMoreButtonTemplate} from "./view/button-load";
 import {generateTask} from "./mock/task";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 4;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 
@@ -26,9 +26,9 @@ const boardElement = siteMainElement.querySelector(`.board`);
 const tasksElement = boardElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingTemplate(), `afterbegin`);
-render(tasksElement, createTaskEditorTemplate(), `beforeend`);
+render(tasksElement, createTaskEditorTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
+for (let i = 1; i < TASK_COUNT; i++) {
   render(tasksElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
