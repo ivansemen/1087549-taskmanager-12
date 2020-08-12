91e64c1c1c254b7cc57c0c4626b91291f6c6513b
Ivan <vsem2004@gmail.com>
2020-08-13 00:23:24 +0300

    M3T1 Described the logic for displaying recurrence days

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -10,6 +10,10 @@ const isExpired = (dueDate) => {
   return currentDate > dueDate.getTime();
 };
 
+const isRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
+
 const createTaskEditorDateTemplate = (dueDate) => {
   return `<button class="card__date-deadline-toggle" type="button">
       date: <span class="card__date-status">${dueDate !== null ? `yes` : `no`}</span>
@@ -26,6 +30,27 @@ const createTaskEditorDateTemplate = (dueDate) => {
       </label>
     </fieldset>` : ``}
   `;
+};
+
+const createTaskEditorRepeatingTemplate = (repeating) => {
+  return `<button class="card__repeat-toggle" type="button">
+    repeat:<span class="card__repeat-status">${isRepeating(repeating) ? `yes` : `no`}</span>
+  </button>
+  ${isRepeating(repeating) ? `<fieldset class="card__repeat-days">
+    <div class="card__repeat-days-inner">
+      ${Object.entries(repeating).map(([day, repeat]) => `<input
+        class="visually-hidden card__repeat-day-input"
+        type="checkbox"
+        id="repeat-${day}"
+        name="repeat"
+        value="${day}"
+        ${repeat ? `checked` : ``}
+      />
+      <label class="card__repeat-day" for="repeat-${day}"
+        >${day}</label
+      >`).join(``)}
+    </div>
+  </fieldset>` : ``}`;
 };
 
 
@@ -50,9 +75,10 @@ export const createTaskEditorTemplate = (task = {}) => {
     ``;
   const dateTemplate = createTaskEditorDateTemplate(dueDate);
 
-  const repeatingClassName = Object.values(repeating).some(Boolean) ?
-    `card--repeat` :
-    ``;
+  const repeatingClassName = isRepeating(repeating)
+    ? `card--repeat`
+    : ``;
+  const repeatingTemplate = createTaskEditorRepeatingTemplate(repeating);
 
   return `<article class="card card--edit card--${color} ${deadlineClassName} ${repeatingClassName}">
       <form class="card__form" method="get">
@@ -75,86 +101,7 @@ export const createTaskEditorTemplate = (task = {}) => {
             <div class="card__details">
               <div class="card__dates">
                  ${dateTemplate}
-                <button class="card__repeat-toggle" type="button">
-                  repeat:<span class="card__repeat-status">yes</span>
-                </button>
-                <fieldset class="card__repeat-days">
-                  <div class="card__repeat-days-inner">
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      id="repeat-mo-4"
-                      name="repeat"
-                      value="mo"
-                    />
-                    <label class="card__repeat-day" for="repeat-mo-4"
-                      >mo</label
-                    >
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      id="repeat-tu-4"
-                      name="repeat"
-                      value="tu"
-                      checked
-                    />
-                    <label class="card__repeat-day" for="repeat-tu-4"
-                      >tu</label
-                    >
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      id="repeat-we-4"
-                      name="repeat"
-                      value="we"
-                    />
-                    <label class="card__repeat-day" for="repeat-we-4"
-                      >we</label
-                    >
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      id="repeat-th-4"
-                      name="repeat"
-                      value="th"
-                    />
-                    <label class="card__repeat-day" for="repeat-th-4"
-                      >th</label
-                    >
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      id="repeat-fr-4"
-                      name="repeat"
-                      value="fr"
-                      checked
-                    />
-                    <label class="card__repeat-day" for="repeat-fr-4"
-                      >fr</label
-                    >
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      name="repeat"
-                      value="sa"
-                      id="repeat-sa-4"
-                    />
-                    <label class="card__repeat-day" for="repeat-sa-4"
-                      >sa</label
-                    >
-                    <input
-                      class="visually-hidden card__repeat-day-input"
-                      type="checkbox"
-                      id="repeat-su-4"
-                      name="repeat"
-                      value="su"
-                      checked
-                    />
-                    <label class="card__repeat-day" for="repeat-su-4"
-                      >su</label
-                    >
-                  </div>
-                </fieldset>
+                ${repeatingTemplate}
               </div>
             </div>
             <div class="card__colors-inner">
