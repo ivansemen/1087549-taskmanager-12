35d757133b572aeb6998be87d153b24a4540e764
Ivan <vsem2004@gmail.com>
2020-08-13 01:08:49 +0300

    M3T1 Described the data structure of filters

--- src/utils.js
+++ src/utils.js
@@ -5,16 +5,31 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+
+  return new Date(currentDate);
+};
+
 export const isTaskExpired = (dueDate) => {
   if (dueDate === null) {
     return false;
   }
 
-  let currentDate = new Date();
-  currentDate.setHours(23, 59, 59, 999);
-  currentDate = new Date(currentDate);
+  const currentDate = getCurrentDate();
 
   return currentDate.getTime() > dueDate.getTime();
+};
+
+export const isTaskExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentDate();
+
+  return currentDate.getTime() === dueDate.getTime();
 };
 
 export const isTaskRepeating = (repeating) => {
@@ -24,3 +39,4 @@ export const isTaskRepeating = (repeating) => {
 export const humanizeTaskDueDate = (dueDate) => {
   return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
 };
+
